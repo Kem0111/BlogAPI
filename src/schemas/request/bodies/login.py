59651734d16f5773480ca0b10cffcda02dc67ab9
Камран <kem@192.168.1.15>
2023-07-04 23:24:07 +0300

    update login schemas

--- src/schemas/request/bodies/login.py
+++ src/schemas/request/bodies/login.py
@@ -1,6 +1,7 @@
-from pydantic import EmailStr, BaseModel
+from pydantic import EmailStr, BaseModel, Field
+from src.metadata import PASSWORD_REGEX
 
 
 class Login(BaseModel):
     email: EmailStr
-    password: str
+    password: str = Field(..., max_length=30, regex=PASSWORD_REGEX)
